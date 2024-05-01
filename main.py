e3da93f24ce475213b881539650ef6d6d78c86e1
the devloper's studios <140197542+Canjicagames@users.noreply.github.com>
2024-05-01 15:56:35 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import pyautogui
+import time
+import webbrowser
+
+
+
+pyautogui.PAUSE = 2
+link = f'https://classic.minecraft.net/'
+webbrowser.open(link)
+pyautogui.sleep(3)
+
+pyautogui.sleep(19)
+
+pyautogui.write("bot python")
+pyautogui.sleep(3)
+pyautogui.click(x=878, y=526)
+pyautogui.sleep(3)
+pyautogui.press("F5")
+pyautogui.sleep(3)
+pyautogui.press("enter")
+
+
+
+
+
